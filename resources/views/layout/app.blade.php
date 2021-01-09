feed803729678b94de6c72db5e2ab1e39b875e86
mvabf <mvabf_dev@outlook.com>
2021-01-09 10:37:53 -0300

    projeto criado, layout app criado usando blade e bootstrap

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{asset('css/app.css') }}">
+    <title>Product Register</title>
+</head>
+<body>
+
+    <div class="container">
+        <main role="main">
+            @hasSection ('body')
+                @yield('body')
+            @endif
+        </main>
+    </div>
+    
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
